1d18aa08a2822315d547aa7ddd13861be8df7afb
Michael Marlowe <marlowemich@gmail.com>
2022-04-14 20:13:17 -0400

    All buttons work on all pages. Picture comes up on individual plant pages. Delete buttons styled. Site navigates smoothly

--- public/js/plantFunc.js
+++ public/js/plantFunc.js
@@ -6,17 +6,12 @@ const singlePost = document.querySelectorAll('.singlePost')
 // function fetchyBoy(){
 for (let i=0; i<singlePost.length; i++){
 
-// let plantName = document.getElementById("plantName");
-// let plantImg = document.getElementById("plantImg");
-
-// console.log(singlePost[0].children[2])
-
 let url =
   "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&list=search&srsearch=" +
-  singlePost[i].children[2].textContent.toLowerCase();
+  singlePost[i].children[3].textContent.toLowerCase();
 let imgUrl =
   "http://en.wikipedia.org/w/api.php?action=query&origin=*&prop=pageimages&format=json&piprop=original&pilicense=any&titles=" +
-  singlePost[i].children[2].textContent.toLowerCase();
+  singlePost[i].children[3].textContent.toLowerCase();
 fetch(url)
   .then(function (response) {
     return response.json();
@@ -32,7 +27,7 @@ fetch(url)
       .then(function (data) {
         // console.log(data);
         // console.log(data.query.pages[wikiPageId].original.source)
-        singlePost[i].children[0].setAttribute(
+        singlePost[i].children[1].setAttribute(
           "src",
           data.query.pages[wikiPageId].original.source
         );
@@ -44,42 +39,40 @@ fetch(url)
 
 
 
-/*
 
-// const plantName = document.getElementById("plantName");
+// function singlePicture(){
+
+// const plantName = document.getElementById("plantName").value;
 // const plantImg = document.getElementById("plantImg");
 
-document.querySelectorAll(".singlePost").forEach(post=> {
-  // document.write(post);
+//   let url =
+//     "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&list=search&srsearch=" +
+//     plantName.textContent.toLowerCase();
+//   let imgUrl =
+//     "http://en.wikipedia.org/w/api.php?action=query&origin=*&prop=pageimages&format=json&piprop=original&pilicense=any&titles=" +
+//     plantName.textContent.toLowerCase();
+//   fetch(url)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       // console.log(data.query.search[0].pageid)
+//       console.log(data);
+//       let wikiPageId = data.query.search[0].pageid;
+//       return fetch(imgUrl)
+//         .then(function (repsonse) {
+//           return repsonse.json();
+//         })
+//         .then(function (data) {
+//           console.log(data);
+//           // console.log(data.query.pages[wikiPageId].original.source)
+//           plantImg.setAttribute(
+//             "src",
+//             data.query.pages[wikiPageId].original.source
+//           );
+//         });
+//     });
 
-  let url =
-    "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&list=search&srsearch=" +
-    plantName.textContent.toLowerCase();
-  let imgUrl =
-    "http://en.wikipedia.org/w/api.php?action=query&origin=*&prop=pageimages&format=json&piprop=original&pilicense=any&titles=" +
-    plantName.textContent.toLowerCase();
-  fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      // console.log(data.query.search[0].pageid)
-      console.log(data);
-      let wikiPageId = data.query.search[0].pageid;
-      return fetch(imgUrl)
-        .then(function (repsonse) {
-          return repsonse.json();
-        })
-        .then(function (data) {
-          console.log(data);
-          // console.log(data.query.pages[wikiPageId].original.source)
-          plantImg.setAttribute(
-            "src",
-            data.query.pages[wikiPageId].original.source
-          );
-        });
-    });
+//   }
 
-});
-
-*/
+//   document.onload(singlePicture);
